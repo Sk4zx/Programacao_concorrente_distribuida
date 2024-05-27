c1a87f05cc06751b0ea0dc63deca037cba21c219
RENAN DA COSTA E SILVA SOUSA <renan.csousa@a.ucb.br>
2024-05-27 10:19:28 -0300

    solucao socket

---
+++ trabalho_individual_2/Client.java
@@ -0,0 +1,16 @@
+package trabalho_individual_2;
+
+public class Client{
+
+    private static final String IP = "localhost";
+
+    public static void main(String[] args) {
+        
+        System.out.println("alooo");
+
+
+
+
+    }
+
+}
